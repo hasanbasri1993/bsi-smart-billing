3c6f70de6f0e0040114a38da21a719fbdef82642
Hasan Basri <hasanbasri1493@gmail.com>
2025-02-15 11:59:40 +0700

    prepare for web sracp bpi
    - getToken

--- config/bsi-smart-billing.php
+++ config/bsi-smart-billing.php
@@ -5,4 +5,7 @@ return [
     'url' => env('BSI_SMARTBILLING_URL', 'https://sandbox.smartbilling.co.id/api/v1/'),
     'client_id' => env('BSI_SMARTBILLING_CLIENT_ID'),
     'client_secret' => env('BSI_SMARTBILLING_CLIENT_SECRET'),
+    'web_url' => env('BPI_WEB_URL', 'https://sandbox.api.bpi.co.id/api/smartbilling/'),
+    'username' => env('BPI_USERNAME', ''),
+    'password' => env('BPI_PASSWORD', ''),
 ];
